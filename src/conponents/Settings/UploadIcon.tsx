1724007c997990ed4d2b331d089322814c05bcdc
kapkande <kap.kande@gmail.com>
2023-12-26 16:39:59 +0300

    init: add drag loader

--- src/conponents/Settings/UploadIcon.tsx
+++ src/conponents/Settings/UploadIcon.tsx
@@ -1,12 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { loginVerification } from "../post/loginVerification";
 import styles from "./Settings.module.css"
 import axios from "axios";
 import { linkInBack } from "../linkInBack";
 
 export default function UploadIcon() {
-    // const [isAdmin, setAdmin] = useState(false);
-    // const [isUser, setUser] = useState(false);
+    const [isLoad, setLoad] = useState(false);
+
+    const [isHover, setHover] = useState(false);
+    const [isBackgroundHover, setBackgroundHover] = useState(false);
+    const [isBackgroundImageHover, setBackgroundImageHover] = useState(false);
+    const [isImg, setImg] = useState(false);
+
+    const ref = useRef(null)
     const [selectedImage, setSelectedImage] = useState<File | null>(null);
     const link: string = `${linkInBack}/load/uploadIcon`;
     const [userData, setUserData] = useState({
@@ -28,27 +34,96 @@ export default function UploadIcon() {
 
         const formData = new FormData()
         formData.append('file', selectedImage)
-
-        await axios.post(link, formData, {
-            headers: {
-                tocen: tocen,
-                userName: userData.name
+        const config = {
+            onUploadProgress: function (progressEvent: any) {
+                setLoad(true)
+                const percentCompleted = Math.round((progressEvent.loaded / progressEvent.total) * 100);
+                console.log(percentCompleted);
+                if (percentCompleted === 100) {
+                    location.reload();
+                    setLoad(false)
+                }
             },
-        })
-            .then((r) => {
-                console.log(r);
-            })
+        };
+
+        try {
+            const response = await axios.post(link, formData, {
+                headers: {
+                    tocen: tocen,
+                    userName: userData.name,
+                },
+                onUploadProgress: config.onUploadProgress,
+            });
+
+            console.log('Upload Successful', response);
+        } catch (error) {
+            console.error('Upload Failed', error);
+        }
     }
+
+    const setHovers = (value: boolean) => {
+        setHover(value);
+        setBackgroundHover(value);
+        setBackgroundImageHover(value);
+        setImg(value);
+    }
+
+    const dragOverEnterLeav = (e: React.DragEvent) => {
+        e.preventDefault();
+        setHovers(true);
+    }
+
+
+    const fileDrop = (e: React.DragEvent) => {
+        e.preventDefault();
+        setHovers(false);
+        const file = (e.dataTransfer.files && e.dataTransfer.files[0]) as File;
+        if (['image/png', 'image/jpeg', 'image/gif', 'image/svg+xml'].includes(file.type)) {
+            console.log('Valid image file:', file);
+            setSelectedImage(file);
+        } else {
+            console.log('Invalid file type. Please select a valid image file.');
+        }
+    }
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = (e.target.files && e.target.files[0]) as File;
+        console.log(file);
+        setSelectedImage(file);
+
+    }
+
     return (
         <div>
-
             <div>
-                <img
-                    alt="icon"
-                    width={"250px"}
-                    src={`${linkInBack}/load/icon/${userData.name}`}
-                />
-                <br />
+                <label className={isHover ? `${styles.usersImageHover} ${styles.usersImage}` : styles.usersImage} htmlFor="userImage"
+                    onDragOver={dragOverEnterLeav}
+                    onDragEnter={dragOverEnterLeav}
+                    onDragLeave={dragOverEnterLeav}
+                    onDrop={fileDrop}
+                >
+                    <input
+                        className={styles.input}
+                        ref={ref}
+                        id="userImage"
+                        type="file"
+                        accept="image/*,.png,.jpg,.gif"
+                        onChange={onChange}
+                    />  <img
+                        className={isImg ? `${styles.imgHover} ${styles.img}` : styles.img}
+                        alt="icon"
+                        width={"250px"}
+                        src={`${linkInBack}/load/icon/${userData.name}`}
+                    />
+                    <div className={isBackgroundHover ? `${styles.backgroundHover} ${styles.background}` : styles.background}>
+                        <div className={isBackgroundImageHover ? `${styles.backgroundImageHover} ${styles.backgroundImage}` : styles.backgroundImage}></div>
+                        <p>Drop & drag files here</p>
+                        <p>or</p>
+                        <p className={styles.text}>Browse Files</p>
+                    </div>
+
+                </label>
+
                 {selectedImage &&
                     <>
                         <button className={styles.button} onClick={() => setSelectedImage(null)}>Remove</button>
@@ -57,15 +132,6 @@ export default function UploadIcon() {
                 }
             </div>
 
-
-            <input
-                type="file"
-                accept="image/*,.png,.jpg,.gif"
-                onChange={(event) => {
-                    const file = (event.target.files && event.target.files[0]) as File;
-                    setSelectedImage(file);
-                }}
-            />
         </div>
     )
 }
